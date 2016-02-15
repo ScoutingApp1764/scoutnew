2afd84433d670281db766f09f7142370267c374d
ScoutingApp1764 <aguyshat@gmail.com>
2016-02-15 15:31:22 -0600

    5 minutes passed (gitlazy.sh)

--- scoutmaster.py
+++ scoutmaster.py
@@ -254,18 +254,12 @@ def securityVulnerability():
 	if secureMode:
 		return '<!DOCTYPE html><html><head></head><body><p>Sorry, the host has turned off uploading JSON\'d databases.</p></body></html>'#well, when you put it like that it makes me want to turn it off always
 	#get it out of json
-	jsoned = request.form.get("json")
-	data= json.loads(jsoned) # tuple > list is sql ? format
-	i = -1
-	for listed in data:
-		i=i+1
-		data[i]=tuple(listed)
-
-	data=tuple(data)
-
 	sql = get_db()
 	m=sql.cursor()
-	m.execute("INSERT INTO Data VALUES("+what+")",data)
+	jsoned = request.form.get("json")
+	data= json.loads(jsoned) # tuple > list is sql ? format
+	for listed in data:
+		m.execute("INSERT INTO Data VALUES("+what+")",(listed))
 	
 
 
