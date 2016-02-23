ee8ab406db934ef9e380a492c9e23ed57244fd79
Jackson <jmcn8782.student.liberty.k12.mo.us>
2016-02-23 16:37:16 -0600

    5 minutes passed (gitlazy.sh)

--- scoutmaster.py
+++ scoutmaster.py
@@ -12,9 +12,9 @@ from os import listdir
 #Syntax: Variable Name, type, human name, [radiobutton options]. Variable names really only help with teamNum and if fort debugging via interacting with the database directly
 letable = [
 	["teamNum","updown","Team num","The team number"],
-        ["roundNum","updown","Round Num","The round number"], 
+        ["roundNum","updown","Round Num","The round number"],
 	["isRed","check","Is on red team","Is the robot on the red team?"],
-	["posi","radio","Position","The position--starting from the closest to you and going to the farthest--the robot is in",["Position 1","Position 2","Position 3"]], 
+	["posi","radio","Position","The position--starting from the closest to you and going to the farthest--the robot is in",["Position 1","Position 2","Position 3"]],
 	["autoHead","head","Autonomous"],
 	["sallyScrossed","check","Crossed Sallyport","Wheather the robot crossed the sallyport in autonomous"],
 	["drawCrossed","check","Crossed drawbridge","Wheather the robot crossed the drawbridge in autonomous"],
@@ -70,12 +70,12 @@ for arg in sys.argv:
 		m.execute("DROP TABLE IF EXISTS Data")
 		s.commit()
 		print("Database cleared. Recreating template and quitting.")
-		
+
 		strTable = ""
 		for _list in letable:
 			if _list[1] == "text":
 				strTable = strTable+_list[0]+" TEXT,"
-			else: 
+			else:
 				strTable = strTable+_list[0]+" INT,"
 		m.executescript("CREATE TABLE Data ("+strTable[0:-1]+");")
 		s.commit()
@@ -90,7 +90,7 @@ Options:
 -spam	Create a 200 entries of randomized scouting data
 -xxs	Debug option to host on a different port to test uploading databases
 -d	Debug mode. You want this if you're modifying code.'''
-	
+
     elif arg == "-s": #secure mode
 	secureMode = True #basically, just turns off the ability to upload SQL databases--useful if you where to be running this on a non-secure network
     elif arg == "-p":
@@ -106,7 +106,7 @@ Options:
 		cData = []
 		for item in letable:
 			if item[0] == "teamNum":
-				cData.append(random.randrange(1000)) 
+				cData.append(random.randrange(1000))
 			elif item[1] == "radio":
 				cData.append(random.randrange(len(item[4])-1))
 			elif item[1] == "text":
@@ -123,24 +123,24 @@ Options:
 	s.commit()
 	s.close()
 
-		
-	
-		
-		
+
+
+
+
 if not doNotStart:
 	from flask import Flask,request,send_file,render_template,g,Response
 	app = Flask(__name__)
 	def get_db():
 	    sql = getattr(g, 'MASTERDB', None)
 	    if sql is None:
-		sql = g._database = sqlite3.connect("MASTERDB") 
+		sql = g._database = sqlite3.connect("MASTERDB")
 		return sql
 
-	@app.route('/')
-	def clientEnd():
+	@app.route('/<evil>')
+	def clientEnd(evil = False):
 		if paranoidMode:
 			return '<!DOCTYPE html><html><head><meta http-equiv=\"refresh\" content=\"0;url=/master\"></head><body><p>Sorry, the host has turned submitting data off.</p></body></html>'
-		return render_template('client.html',letable=letable)
+		return render_template('client.html',letable=letable,evil = evil)
 
 
 	#this will be used in clientSubmit and in the sql submitting
@@ -166,51 +166,51 @@ if not doNotStart:
 			#except:
 			#	return "<p>Something went wrong with the data. Did you enter text in number boxes? Try hitting the back arrow and resubmitting the data, without text in number boxes.</p><!--you've not caused database errors, the database is sanitized"
 		#insert into db
-		
+
 		sql=get_db()
 		m=sql.cursor()
 		m.executemany("INSERT INTO Data VALUES("+what+")",[tuple(res)])
 		sql.commit()
 
-	#m.executemany("INSERT INTO Data VALUES("+what+")",res)	
-		return "<!DOCTYPE html><html><head><meta http-equiv=\"refresh\" content=\"0;url=/#top\"></head><body><p>Hit the back button on your browser. The redirect failed, however your scouting data was submitted successfully.</p></body></html>" #doesn't really need to be its own file or template. 
+	#m.executemany("INSERT INTO Data VALUES("+what+")",res)
+		return "<!DOCTYPE html><html><head><meta http-equiv=\"refresh\" content=\"0;url=/#top\"></head><body><p>Hit the back button on your browser. The redirect failed, however your scouting data was submitted successfully.</p></body></html>" #doesn't really need to be its own file or template.
 
 	@app.route("/master/")
 	def serverEnd():
 		sql=get_db()
 		m=sql.cursor()
-		return render_template("server.html",secureMode=secureMode,letable=letable, teams = m.execute("SELECT DISTINCT teamNum FROM Data").fetchall(),   alls = m.execute("SELECT * FROM Data ORDER BY teamNum").fetchall()) 
+		return render_template("server.html",secureMode=secureMode,letable=letable, teams = m.execute("SELECT DISTINCT teamNum FROM Data").fetchall(),   alls = m.execute("SELECT * FROM Data ORDER BY teamNum").fetchall())
 		s.close()
 	@app.route('/itemSort/<s>')
 	def itemSort(s):
 		s=int(s) #can never be too safe
 		sql=get_db()
 		m=sql.cursor()
-		return render_template("server.html",secureMode = secureMode,s=s,itemSort = True,letable=letable, teams = m.execute("SELECT DISTINCT teamNum FROM Data").fetchall(),   alls = m.execute("SELECT teamNum, "+letable[s][0]+" FROM Data ORDER BY teamNum").fetchall()) 
+		return render_template("server.html",secureMode = secureMode,s=s,itemSort = True,letable=letable, teams = m.execute("SELECT DISTINCT teamNum FROM Data").fetchall(),   alls = m.execute("SELECT teamNum, "+letable[s][0]+" FROM Data ORDER BY teamNum").fetchall())
 	@app.route('/itemSortLarge/<s>')
 	def itemSort2(s):
 		s=int(s) #can never be too safe
 		sql=get_db()
 		m=sql.cursor()
-		return render_template("server.html",secureMode = secureMode,s=s,itemSort = True,letable=letable, teams = m.execute("SELECT DISTINCT teamNum FROM Data").fetchall(),   alls = m.execute("SELECT teamNum, "+letable[s][0]+" FROM Data ORDER BY "+letable[s][0]+"").fetchall()) 
-		
+		return render_template("server.html",secureMode = secureMode,s=s,itemSort = True,letable=letable, teams = m.execute("SELECT DISTINCT teamNum FROM Data").fetchall(),   alls = m.execute("SELECT teamNum, "+letable[s][0]+" FROM Data ORDER BY "+letable[s][0]+"").fetchall())
 
 
-		
+
+
 
 	@app.route('/teamSort/<s>')
 	def teamSort(s):
 		s=int(s) #can never be too safe
 		sql=get_db()
 		m=sql.cursor()
-		return render_template("server.html",secureMode = secureMode,letable=letable, teams = m.execute("SELECT DISTINCT teamNum FROM Data").fetchall(),   alls = m.execute("SELECT * FROM Data WHERE teamNum = "+str(s)+" ORDER BY teamNum").fetchall()) 
+		return render_template("server.html",secureMode = secureMode,letable=letable, teams = m.execute("SELECT DISTINCT teamNum FROM Data").fetchall(),   alls = m.execute("SELECT * FROM Data WHERE teamNum = "+str(s)+" ORDER BY teamNum").fetchall())
 
 
 
 
 	#for ty in letable:
 	#	for 6:
-	#		addavg data[i][o] 	
+	#		addavg data[i][o]
 	@app.route("/avg/<s>")
 	def brilliance(s,functioned = False):
 		s=int(s) #can never be too safe
@@ -232,7 +232,7 @@ if not doNotStart:
 				if ty[1] == "radio":
 					#Go to the radio button's table, then go to that specific radio button option that was checked this time arround
 					#Add a 1 divided by the total ammount of radio buttons to it if it was checked this time
-					#We get which radio button was checked by the value of the the radio button 
+					#We get which radio button was checked by the value of the the radio button
 					try:
 						fakeall[i][teamall[o][i]] = fakeall[i][teamall[o][i]] + (1.0/length)
 					except:
@@ -246,7 +246,7 @@ if not doNotStart:
 						fakeall[i] = "An error occured while trying to average this data"
 		if functioned:
 			return fakeall
-		return render_template("server.html",secureMode = secureMode,avg=True,letable=letable, teams = m.execute("SELECT DISTINCT teamNum FROM Data").fetchall(), alls = [fakeall]) 
+		return render_template("server.html",secureMode = secureMode,avg=True,letable=letable, teams = m.execute("SELECT DISTINCT teamNum FROM Data").fetchall(), alls = [fakeall])
 
 	@app.route("/allavg/")
 	def allavg():
@@ -256,7 +256,7 @@ if not doNotStart:
 		fakealls=[]
 		for team in teams:
 			fakealls.append(brilliance(team[0],True))
-		
+
 		return render_template("server.html",secureMode = secureMode,avg=True,letable=letable, teams = m.execute("SELECT DISTINCT teamNum FROM Data").fetchall(), alls = fakealls)
 
 
@@ -286,7 +286,7 @@ if not doNotStart:
 		minmax = "min"
 		if _isMax:
 			minmax = "max"
-		return render_template("server.html",secureMode = secureMode,minmax=minmax,letable=letable, teams = m.execute("SELECT DISTINCT teamNum FROM Data").fetchall(), alls = [fakeall]) 
+		return render_template("server.html",secureMode = secureMode,minmax=minmax,letable=letable, teams = m.execute("SELECT DISTINCT teamNum FROM Data").fetchall(), alls = [fakeall])
 	@app.route("/max/<s>")
 	def _max(s):
 		return _min(s,True)
@@ -295,7 +295,7 @@ if not doNotStart:
 		sql = get_db()
 		m=sql.cursor()
 		sqall = m.execute("SELECT * FROM Data").fetchall()
-		return json.dumps(sqall,sort_keys = False)		
+		return json.dumps(sqall,sort_keys = False)
 
 	@app.route("/exportdb/")
 	def exportdb():
@@ -313,11 +313,11 @@ if not doNotStart:
 		#upload our database to the master
 		if request.method == "POST":
 			ip = request.form.get("ip")
-			json = jsondb()			
+			json = jsondb()
 			return render_template("uploaddb_ok.html",ip=ip,json=jsoned)
 
 		return render_template("uploaddb.html")
-			
+
 	def jsonToSql(jsoned):
 		#get it out of json
 		sql = get_db()
@@ -333,9 +333,9 @@ if not doNotStart:
 	def importDbs():
 		if secureMode:
 			return '<!DOCTYPE html><html><head></head><body><p>Sorry, the host has turned off uploading JSON\'d databases.</p></body></html>'#well, when you put it like that it makes me want to turn it off always
-			
+
 		location = "rawDatabases/"
-		print("list " + str(listdir(location)))	
+		print("list " + str(listdir(location)))
 		for tfile in listdir(location):
 			if tfile[0:1] != ".":
 				_file = open(location + tfile)
@@ -343,7 +343,7 @@ if not doNotStart:
 				_file.close()
 
 
-		return "<!DOCTYPE html><html><head><meta http-equiv=\"refresh\" content=\"0;url=/master/\"></head><body><p>Hit the back button on your browser. The redirect failed, however your scouting data was submitted successfully.</p></body></html>" #doesn't really need to be its own file or template. 
+		return "<!DOCTYPE html><html><head><meta http-equiv=\"refresh\" content=\"0;url=/master/\"></head><body><p>Hit the back button on your browser. The redirect failed, however your scouting data was submitted successfully.</p></body></html>" #doesn't really need to be its own file or template.
 
 
 	@app.route("/uploaddb_up/",methods=["POST"])
@@ -353,10 +353,10 @@ if not doNotStart:
 			return '<!DOCTYPE html><html><head></head><body><p>Sorry, the host has turned off uploading JSON\'d databases.</p></body></html>'#well, when you put it like that it makes me want to turn it off always
 		jsoned = request.form.get("json")
 		jsontoSql(jsoned)
-		return "<!DOCTYPE html><html><head><meta http-equiv=\"refresh\" content=\"0;url=/#top\"></head><body><p>Hit the back button on your browser. The redirect failed, however your scouting data was submitted successfully.</p></body></html>" #doesn't really need to be its own file or template. 
+		return "<!DOCTYPE html><html><head><meta http-equiv=\"refresh\" content=\"0;url=/#top\"></head><body><p>Hit the back button on your browser. The redirect failed, however your scouting data was submitted successfully.</p></body></html>" #doesn't really need to be its own file or template.
 
 
-	
+
 	def servefile(path,mime):
 		_file = open(path,"r")
 		_res = _file.read()
@@ -369,7 +369,7 @@ if not doNotStart:
 
 	@app.route("/Chart.js")
 	def chartjs():
-		return servefile("Chart.js","text/javascript")	
+		return servefile("Chart.js","text/javascript")
 	@app.route("/bootstrap.min.js")
 	def bootJs():
 		return servefile("bootstrap.min.js","text/javascript")
@@ -379,7 +379,7 @@ if not doNotStart:
 	@app.route("/jquery.min.js")
 	def jqJs():
 		return servefile("jquery.min.js","text/javascript")
-	
+
 
 
 
@@ -388,5 +388,5 @@ if not doNotStart:
 		port =	9001
 		if xxs:
 			port = 83
-		
+
 		app.run(debug=debug, host='0.0.0.0',port=port)
