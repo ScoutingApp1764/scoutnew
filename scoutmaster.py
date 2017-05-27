6f951492c56cda7734f7d31541fe076b9c20d9b4
AI221 <aguyshat@gmail.com>
2017-05-27 11:45:44 -0500

    a commit message

--- scoutmaster.py
+++ scoutmaster.py
@@ -10,75 +10,34 @@ from os import listdir
 
 
 #Syntax: Variable Name, type, human name, [radiobutton options]. Variable names really only help with teamNum and if fort debugging via interacting with the database directly
-letable = [
-	["teamNum","updown","Team num","The team number"],
-	["isRed","check","Is on red team","Is the robot on the red team?"],
-	["posi","radio","Position","The position--starting from the closest to you and going to the farthest--the robot is in",["Position 1","Position 2","Position 3"]],
-    ["noshow","check","No show","Robot did not show for this competition"],
 
+categorySorter = "Team Number"
 
-	["autoHead","head","Autonomous"],
-
-
-    ["Apicsally","pic","sallyport.png"],
-	["sallyScrossed","check","Crossed Sallyport","Wheather the robot crossed the sallyport in autonomous"],
-	["Apicrough","pic","rough.png"],
-	["roughCrossed","check","Crossed rough terrain","Wheather the robot crossed the rought terrain in autonomous"],
-	["Apicstone","pic","stone.png"],
-	["stoneCrossed","check","Crossed rock wall","Wheather the robot crossed the stone wall in autonomous"],
-	["Apicmoat","pic","moat.png"],
-	["moatCrossed","check","Crossed moat","Wheather the robot crossed the moat in autonomous"],
-	["Apicsaw","pic","cheval.png"],
-	["sawCrossed","check","Crossed Cheval de Frise","Wheather the robot crossed the seasaw in autonomous"],
-	["Apiclow","pic","lowbar.png"],
-	["lowCrossed","check","Crossed lowbar","Wheather the robot crossed the lowbar in autonomous"],
-	["Apicramp","pic","ramparts.png"],
-	["rampCrossed","check","Crossed ramparts","Wheather the robot crossed the ramp in autonomous"],
-	["Apicgate","pic","portcullis.png"],
-	["gateCrossed","check","Crossed portcullis", "Wheather the robot crossed the gate in autonomous"],
-	["Apicdrawm","pic","drawbridge.png"],
-	["drawCrossed","check","Crossed drawbridge","Wheather the robot crossed the drawbridge in autonomous"],
-	["pickedBall","check","Picked up a ball","Did the robot pick up a ball in autonomous?"],
-	["startedBall","check","Started w/ ball","Did the robot start with a ball in autonomous?"],
-	["shotBall","radio","Shot ball","Did the robot shoot a ball in autonomous? Where?",["Didn't","Low","High"]],
-	["wasntABrick","check","Moved to ramp","Did the robot move to the ramp in autonomous?"],
-
-
-	["teleHead","head","Teleop"],
-
-
-    ["picsally","pic","sallyport.png"],
-	["teleCSally","updown","Crossed sallyport","Number of times the robot crossed the sallyport in teleop"],
-	["picrough","pic","rough.png"],
-	["teleCRough","updown","Crossed rough terrain","Number of times the robot crossed the rough terrain in teleop"],
-	["picstone","pic","stone.png"],
-	["teleCStone","updown","Crossed rock wall","Number of times the robot crossed the rock wall in teleop"],
-	["picmoat","pic","moat.png"],
-	["teleCMoat","updown","Crossed moat","Number of times the robot crossed the moat in teleop"],
-	["picsaw","pic","cheval.png"],
-	["teleCSaw","updown","Crossed Cheval de Frise","Number of times the robot crossed the Cheval de Frise(the one with the four ramps) in teleop"],
-	["piclow","pic","lowbar.png"],
-	["teleCLow","updown","Crossed lowbar","Number of times the robot crossed the lowbar in teleop"],
-	["picramp","pic","ramparts.png"],
-	["teleCRamp","updown","Crossed ramparts","Number of times the robot crossed the ramparts in teleop"],
-	["picgate","pic","portcullis.png"],
-	["teleCGate","updown","Crossed portcullis","Number of times the robot crossed the portcullis  in teleop"],
-	["picdrawm","pic","drawbridge.png"],
-    ["teleCDraw","updown","Crossed drawbridge","Number of times the robot crossed the gate in teleop"],
-	["ballsPicked","updown","Balls picked up","Number of times the robot picked up a ball in teleop"],
-	["lowScored","updown","High scored","Number of times the robot scored in the high goal in teleop"],
-	["highScored","updown","Low scored","Number of times the robot scored in the low goal in teleop"],
-	["gaveBall","updown","Number of balls given to teammate","Did the robot give a ball to another teammate?"],
-	["recBall","updown","Number of balls recieved from team","Did the robot recieve a ball from another teammate?"],
-	["scaledTower","check","Climbed tower","Did the robot climb the tower during the last 30 seconds?"],
-	["manipFail","check","Manipulator failure","Did the robot's manipulator fail technically or physically (not an operator error) during operation?"],
-	["explodes","check","Spontaneous combustion","Did the robot sustain battery damaged, sparking wires, or fire during operation?"],
-	["gotStuck","check","Got stuck","Did the robot get itself in a position where it struggled significantly or did not get out of?"],
-	["paperweight","check","Didn't move at all","Was the robot a paperweight?"],
-    ["stoppedMoving","check","Stopped moving","Did the robot stop moving for a significant period of time"],
-    ["challtower","check","Challenged tower","Did the robot challenge the tower?"],
-    ["climbedtower","check","Climbed tower","Did the robot climb the tower at the last 15 seconds?"],
+layout = {
+	"Team Number":{"type":"forumElement","tooltip":"The number of the team you are scouting."},
+	"Test":{"type":"forumElement","style":"color: #00ff00;"},
+	"Text":{"type":"text","text":"Error: The forum could not be loaded. Please contact your scouting lead.", "style":"color:#00ff00;"},
+	"lol":{"type":"pic","image":"lowbar.png"},
+	"checktest":{"type":"forumElement"},
+	"texttest":{"type":"forumElement"},
+	"radiotest":{"type":"forumElement"},
+}
+layoutOrder = [
+	"Team Number","Test","Text","lol","checktest","texttest","radiotest","noexist",
 ]
+
+
+formtable = {
+	"Team Number":{"type":"updown","ordernum":0},
+	"checktest":{"type":"check","ordernum":1},
+	"texttest":{"type":"text","ordernum":2},
+	"radiotest":{"type":"radio","ordernum":3,"buttons":["first btn","2nd btn","3rd btn"]},
+
+} 
+forumOrder = [
+	"Team Number","checktest","texttest","radiotest"
+]
+
 def clrdb():
 	s = sqlite3.connect("MASTERDB")
 	m = s.cursor()
@@ -86,19 +45,26 @@ def clrdb():
 	s.commit()
 
 	strTable = ""
-	for _list in letable:
-		if _list[1] == "text":
-			strTable = strTable+_list[0]+" TEXT,"
+	for name in forumOrder:
+		if formtable[name]["type"] == "text":
+			strTable = strTable+"["+name+"]"+" TEXT,"
 		else:
-			strTable = strTable+_list[0]+" INT,"
+			strTable = strTable+"["+name+"]"+" INT,"
 	m.executescript("CREATE TABLE Data ("+strTable[0:-1]+");")
 	s.commit()
 	s.close()
 
-what = ""
-for _ in letable:
-	what = what+"?,"
-what = what[0:-1]
+global sql_question_marks
+sql_question_marks = ""
+def gen_sql_question_marks():
+	for _ in formtable:
+		global sql_question_marks
+		sql_question_marks = sql_question_marks+"?,"
+	sql_question_marks = sql_question_marks[0:-1]
+
+gen_sql_question_marks()
+
+
 doNotStart = False
 secureMode = False
 paranoidMode = False
@@ -107,46 +73,46 @@ debug = False
 masterMode = False
 host = '0.0.0.0'
 for arg in sys.argv:
-    if arg == "-c":
+	if arg == "-c":
 		doNotStart = True
 		print"Really clear database? [y/n]"
 		if raw_input() == "y":
 			clrdb()
 			print("Database cleared. Recreating template and quitting.")
-    elif arg == "-m":
-	masterMode = True
-    elif arg == "--help":
+	elif arg == "-m":
+		masterMode = True
+	elif arg == "--help":
 
 		doNotStart = True
 		print'''Usage: sudo python scoutmaster.py
 Options:
 -c	Clear the database
 -s	Secure mode--No uploading databases
--p 	Paranoid mode--No uploading or using the client
+-p	Paranoid mode--No uploading or using the client
 -spam	Create a 200 entries of randomized scouting data
 -xxs	Debug option to host on a different port to test uploading databases
 -d	Debug mode. You want this if you're modifying code.
 -m	Master mode. Disables clear and disables uploading the database to somewhere else'''
 
-    elif arg == "-s": #secure mode
+	elif arg == "-s": #secure mode
 		masterMode = True
 		secureMode = True #basically, just turns off the ability to upload SQL databases--useful if you where to be running this on a non-secure network
-    elif arg == "-p":
+	elif arg == "-p":
 		masterMode = True
 		secureMode = True #This is for running this specifically somewhere you really don't want people adding stuff to the database--say you are showing everyone your scouting data.
 		paranoidMode = True
-    elif arg == "-xxs":
+	elif arg == "-xxs":
 		xxs = True
-    elif arg == "-d":
+	elif arg == "-d":
 		debug = True
 		host = '127.0.0.1'
-    elif arg == "-spam":
+	elif arg == "-spam":
 		doNotStart = True
 		allData =[]
 		for _ in range(0,200):
 			cData = []
-			for item in letable:
-				if item[0] == "teamNum":
+			for item in formtable:
+				if item[0] == "["+categorySorter+"]":
 					cData.append(random.randrange(1000))
 				elif item[1] == "radio":
 					cData.append(random.randrange(len(item[4])-1))
@@ -159,7 +125,7 @@ Options:
 			allData.append(tuple(cData))
 		s = sqlite3.connect("MASTERDB")
 		m = s.cursor()
-		m.executemany("INSERT INTO Data VALUES ("+what+")",allData)
+		m.executemany("INSERT INTO Data VALUES ("+sql_question_marks+")",allData)
 		m.close()
 		s.commit()
 		s.close()
@@ -172,9 +138,9 @@ if not doNotStart:
 	from flask import Flask,request,send_file,render_template,g,Response,send_from_directory
 	app = Flask(__name__)
 	def get_db():
-	    sql = getattr(g, 'MASTERDB', None)
-	    if sql is None:
-		sql = g._database = sqlite3.connect("MASTERDB")
+		sql = getattr(g, 'MASTERDB', None)
+		if sql is None:
+			sql = g._database = sqlite3.connect("MASTERDB")
 		return sql
 
 	@app.route('/')
@@ -187,12 +153,12 @@ if not doNotStart:
 			return '<!DOCTYPE html><html><head><meta http-equiv=\"refresh\" content=\"0;url=/master\"></head><body><p>Sorry, the host has turned submitting data off.</p></body></html>'
 		elif request.method == "POST":
 			res = []
-			i = -1
-			for ty in letable:
-				i = i +1
+			i =-1
+			for name in forumOrder:
+				i=i+1
 				#try:
-				var = request.form.get(str(i))
-				if ty[1] != "text":
+				var = request.form.get(name)
+				if formtable[name]["type"] != "text":
 					try:
 						var = int(var) #make sure no text is put in in place of a number
 					except: #1337 H4X0RS!
@@ -205,9 +171,9 @@ if not doNotStart:
 
 			sql=get_db()
 			m=sql.cursor()
-			m.executemany("INSERT INTO Data VALUES("+what+")",[tuple(res)])
+			m.executemany("INSERT INTO Data VALUES("+sql_question_marks+")",[tuple(res)])
 			sql.commit()
-		return render_template('client.html',letable=letable,evil = evil)
+		return render_template('client.html',formtable=formtable, layout=layout,evil = evil,layoutOrder=layoutOrder)
 	@app.route("/allvilliansarelemons/")
 	def evil():
 		return clientEnd(evil = True)
@@ -219,20 +185,21 @@ if not doNotStart:
 	def serverEnd():
 		sql=get_db()
 		m=sql.cursor()
-		return render_template("server.html",secureMode=secureMode,letable=letable, teams = m.execute("SELECT DISTINCT teamNum FROM Data").fetchall(),   alls = m.execute("SELECT * FROM Data ORDER BY teamNum").fetchall())
+
+		return render_template("server.html",secureMode=secureMode,formtable=formtable, teams = m.execute("SELECT DISTINCT ["+categorySorter+"] FROM Data").fetchall(), alls = m.execute("SELECT * FROM Data ORDER BY ["+categorySorter+"]").fetchall(), layout=layout, forumOrder=forumOrder)
 		s.close()
 	@app.route('/itemSort/<s>')
 	def itemSort(s):
 		s=int(s) #can never be too safe
 		sql=get_db()
 		m=sql.cursor()
-		return render_template("server.html",secureMode = secureMode,s=s,itemSort = True,letable=letable, teams = m.execute("SELECT DISTINCT teamNum FROM Data").fetchall(),   alls = m.execute("SELECT teamNum, "+letable[s][0]+" FROM Data ORDER BY teamNum").fetchall())
+		return render_template("server.html",secureMode = secureMode,s=s,itemSort = True,formtable=formtable, teams = m.execute("SELECT DISTINCT ["+categorySorter+"] FROM Data").fetchall(),   alls = m.execute("SELECT ["+categorySorter+"], "+formtable[s][0]+" FROM Data ORDER BY ["+categorySorter+"]").fetchall(), layout=layout, forumOrder=forumOrder)
 	@app.route('/itemSortLarge/<s>')
 	def itemSort2(s):
 		s=int(s) #can never be too safe
 		sql=get_db()
 		m=sql.cursor()
-		return render_template("server.html",secureMode = secureMode,s=s,itemSort = True,letable=letable, teams = m.execute("SELECT DISTINCT teamNum FROM Data").fetchall(),   alls = m.execute("SELECT teamNum, "+letable[s][0]+" FROM Data ORDER BY "+letable[s][0]+"").fetchall())
+		return render_template("server.html",secureMode = secureMode,s=s,itemSort = True,formtable=formtable, teams = m.execute("SELECT DISTINCT ["+categorySorter+"] FROM Data").fetchall(),   alls = m.execute("SELECT ["+categorySorter+"], "+formtable[s][0]+" FROM Data ORDER BY "+formtable[s][0]+"").fetchall(), layout=layout, forumOrder=forumOrder)
 
 
 
@@ -243,12 +210,12 @@ if not doNotStart:
 		s=int(s) #can never be too safe
 		sql=get_db()
 		m=sql.cursor()
-		return render_template("server.html",secureMode = secureMode,letable=letable, teams = m.execute("SELECT DISTINCT teamNum FROM Data").fetchall(),   alls = m.execute("SELECT * FROM Data WHERE teamNum = "+str(s)+" ORDER BY teamNum").fetchall())
+		return render_template("server.html",secureMode = secureMode,formtable=formtable, teams = m.execute("SELECT DISTINCT ["+categorySorter+"] FROM Data").fetchall(),   alls = m.execute("SELECT * FROM Data WHERE ["+categorySorter+"] = "+str(s)+" ORDER BY ["+categorySorter+"]").fetchall(), layout=layout, forumOrder=forumOrder)
 
 
 
 
-	#for ty in letable:
+	#for ty in formtable:
 	#	for 6:
 	#		addavg data[i][o]
 	@app.route("/avg/<s>")
@@ -256,11 +223,11 @@ if not doNotStart:
 		s=int(s) #can never be too safe
 		sql=get_db()
 		m=sql.cursor()
-		teamall = m.execute("SELECT * FROM Data WHERE teamNum = "+str(s)+" ORDER BY teamNum").fetchall()
+		teamall = m.execute("SELECT * FROM Data WHERE ["+categorySorter+"] = "+str(s)+" ORDER BY ["+categorySorter+"]").fetchall()
 		fakeall = []
 		length = float(len(teamall))
 		i = -1
-		for ty in letable: #go through letable
+		for ty in formtable: #go through formtable
 			i = i+1 # fake a range for loo
 			if ty[1] == "radio": #different method of handling radio buttons
 				fakeall.append([]) # we will have a table of avg times radiobutton was checked
@@ -286,18 +253,18 @@ if not doNotStart:
 						fakeall[i] = "An error occured while trying to average this data"
 		if functioned:
 			return fakeall
-		return render_template("server.html",secureMode = secureMode,avg=True,letable=letable, teams = m.execute("SELECT DISTINCT teamNum FROM Data").fetchall(), alls = [fakeall])
+		return render_template("server.html",secureMode = secureMode,avg=True,formtable=formtable, teams = m.execute("SELECT DISTINCT ["+categorySorter+"] FROM Data").fetchall(), alls = [fakeall], layout=layout, forumOrder=forumOrder)
 
 	@app.route("/allavg/")
 	def allavg():
 		sql = get_db()
 		m=sql.cursor()
-		teams = m.execute("SELECT DISTINCT teamNum FROM Data").fetchall()
+		teams = m.execute("SELECT DISTINCT ["+categorySorter+"] FROM Data").fetchall()
 		fakealls=[]
 		for team in teams:
 			fakealls.append(brilliance(team[0],True))
 
-		return render_template("server.html",secureMode = secureMode,avg=True,letable=letable, teams = m.execute("SELECT DISTINCT teamNum FROM Data").fetchall(), alls = fakealls,allAvg=True)
+		return render_template("server.html",secureMode = secureMode,avg=True,formtable=formtable, teams = m.execute("SELECT DISTINCT ["+categorySorter+"] FROM Data").fetchall(), alls = fakealls,allAvg=True, layout=layout, forumOrder=forumOrder)
 
 
 	@app.route("/min/<s>")
@@ -305,11 +272,11 @@ if not doNotStart:
 		s=int(s) #can never be too safe
 		sql=get_db()
 		m=sql.cursor()
-		teamall = m.execute("SELECT * FROM Data WHERE teamNum = "+str    (s)+" ORDER BY teamNum").fetchall()
+		teamall = m.execute("SELECT * FROM Data WHERE ["+categorySorter+"] = "+str	 (s)+" ORDER BY ["+categorySorter+"]").fetchall()
 		fakeall = []
 		length = len(teamall)
 		i = -1
-		for ty in letable:
+		for ty in formtable:
 			i = i+1
 			if ty[1] == "radio":
 				fakeall.append(-1)
@@ -326,7 +293,7 @@ if not doNotStart:
 		minmax = "min"
 		if _isMax:
 			minmax = "max"
-		return render_template("server.html",secureMode = secureMode,minmax=minmax,letable=letable, teams = m.execute("SELECT DISTINCT teamNum FROM Data").fetchall(), alls = [fakeall])
+		return render_template("server.html",secureMode = secureMode,minmax=minmax,formtable=formtable, teams = m.execute("SELECT DISTINCT ["+categorySorter+"] FROM Data").fetchall(), alls = [fakeall], layout=layout, forumOrder=forumOrder)
 	@app.route("/max/<s>")
 	def _max(s):
 		return _min(s,True)
@@ -342,10 +309,11 @@ if not doNotStart:
 		if secureMode:
 			return render_template("msg.html",msg="Sorry, the host has turned off exporting JSON\'d databases.")
 
-		_file = open("exportedDb_"+str(random.randrange(9999)),"w")
+		randnum = str(random.randrange(999999))
+		_file = open("exportedDb_"+randnum,"w")
 		_file.write(jsondb())
 		_file.close()
-		return render_template("msg.html",msg="Database can be found at exportedDB_ and then a random number")
+		return render_template("msg.html",msg="Database can be found at exportedDB_"+randnum)
 
 
 	@app.route("/uploaddb/",methods=["GET","POST"])
@@ -357,7 +325,7 @@ if not doNotStart:
 		if request.method == "POST":
 			ip = request.form.get("ip")
 			if ip == "127.0.0.1" or ip=="localhost":
-				return render_template("msg.html",msg="Cannot submit toyourself. Find the master IP address.")
+				return render_template("msg.html",msg="Cannot submit to yourself. Find the master IP address.")
 			jsoned = jsondb()
 			return render_template("uploaddb_ok.html",ip=ip,json=jsoned)
 
@@ -369,8 +337,8 @@ if not doNotStart:
 		m=sql.cursor()
 		data= json.loads(jsoned) # tuple > list is sql ? format
 		for listed in data:
-			print("INSERT INTO Data VALUES("+what+")",[tuple(listed)])
-			m.executemany("INSERT INTO Data VALUES("+what+")",[tuple(listed)])
+			print("INSERT INTO Data VALUES("+sql_question_marks+")",[tuple(listed)])
+			m.executemany("INSERT INTO Data VALUES("+sql_question_marks+")",[tuple(listed)])
 		m.close()
 		sql.commit()
 	@app.route("/clear")
@@ -406,6 +374,15 @@ if not doNotStart:
 
 		return "<!DOCTYPE html><html><head><meta http-equiv=\"refresh\" content=\"0;url=/master/\"></head><body><p>Hit the back button on your browser. The redirect failed, however your scouting data was submitted successfully.</p></body></html>" #doesn't really need to be its own file or template.
 
+	@app.route("/layout",methods=['GET','POST'])
+	def modify_layout():
+		return render_template("modify_layout.html",secureMode=secureMode,formtable=formtable,layout=layout)
+
+	@app.route("/layout_preview") #TODO
+	def layout_preview():
+		return "TODO"
+
+
 
 
 	def servefile(path,mime):
@@ -413,8 +390,8 @@ if not doNotStart:
 		_res = _file.read()
 		_file.close()
 		resp = Response(response=_res,
-                	status=200,
-                	mimetype=mime
+					status=200,
+					mimetype=mime
 		)
 		return resp
 
